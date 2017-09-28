aff620ad6b73186dac7cf3964ee13b7953c6fc2c
picrin <Adam Kurkiewicz>
2017-09-28 15:25:41 +0100

    Adding BIO stage 1 competition tasks

---
+++ BIO_2017_stage1/1a_boilerplate.py
@@ -0,0 +1,13 @@
+import sys
+
+# You can use firs_row as a variable containing the colours of sqaures in the first row.
+first_row = sys.stdin.readline()
+
+first_row = first_row.strip()
+
+def compute_color(A, B):
+    # you have to implement me!
+    return ""
+
+# You have to output your answer on the standard output using print
+print("G")
---
+++ BIO_2017_stage1/1a_boilerplate.vb
@@ -0,0 +1,17 @@
+Module Triangles
+
+  Function ComputeColor(Byref A as Char, Byref B as Char) as Char
+    ' you have to implement this function
+    return ""
+  End Function
+
+  Function Main() as Integer
+    ' You can use FirstRow as the variable containing the first row of colours, i.e. RGBGR..
+    Dim FirstRow as String = Console.ReadLine()
+
+    ' You can output your answer, once ready, on the standard output
+    Console.WriteLine("G")
+    return 0
+  End Function
+
+End Module
